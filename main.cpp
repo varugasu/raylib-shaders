e270f14206a745c97f5cf3c1cf8b9d6b1d411306
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2024-09-17 22:32:01 -0300

    chore: initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "raylib.h"
+
+int main(void) {
+  const int screenWidth = 800;
+  const int screenHeight = 450;
+
+  InitWindow(screenWidth, screenHeight, "Shaders");
+
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+
+  return 0;
+}
